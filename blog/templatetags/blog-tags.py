30a786d073e558d2f1e19795ca4635aeeb5d2139
Mohammad Eska <mohammad.eska34@gmail.com>
2025-06-07 12:30:47 +0330

    design the last and popular posts sidebar

--- blog/templatetags/blog-tags.py
+++ blog/templatetags/blog-tags.py
@@ -15,3 +15,13 @@ def function():
 def function(postid):
     post = Post.objects.get(id=postid)
     return post.counted_view
+
+@register.inclusion_tag('blog/blog-popularposts.html')
+def popular_posts():
+    posts = Post.objects.filter(published_date__lte=timezone.now(), status=1).order_by('-counted_view')[:3]
+    return {'popularposts':posts}
+
+@register.inclusion_tag('blog/blog-lastposts.html')
+def last_posts():
+    last_posts = Post.objects.filter(published_date__lte=timezone.now(), status=1).order_by('-published_date')[:3]
+    return {'last_posts':last_posts}
