bff089a59cecca95695adf58ede3cc6b61e69ad4
Mohammad Eska <mohammad.eska34@gmail.com>
2025-07-15 13:51:57 +0330

    stting up login form
    setting up logging form
    using of login situation in page

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
 def login_view(request):
-    return render(request,'accounts/login.html')
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
 
-def logout_view(request):
-    return render(request,'accounts/logout.html')
+    else:
+        msg = "you aren't loggin"
+    return render(request,'accounts/login.html',{'msg':msg})
+
+# def logout_view(request):
+#     return render(request,'accounts/logout.html')
 
 def signup_view(request):
     return render(request,'accounts/signup.html')
